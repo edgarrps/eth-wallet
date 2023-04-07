d5cc54544454997050b5ee73fba159cc74311b72
Edgar Santos <edgarrps@gmail.com>
2023-04-07 18:39:20 -0400

    use client

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,9 +1,13 @@
+'use client'
 import { useEffect } from 'react'
 import Web3 from 'web3'
 
 export default function Wallet() {
     useEffect(() => {
-        const web3 = new Web3(Web3.givenProvider || "wss://eth.llamarpc.com");
+        const web3 = new Web3(Web3.givenProvider || 'wss://eth.llamarpc.com')
+        const wallet = web3.eth.accounts.wallet.create
     }, [])
+
+
     return <></>
 }
