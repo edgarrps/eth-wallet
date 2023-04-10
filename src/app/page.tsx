400878d73f056b99be2c45337be274c1680fb889
Edgar Santos <edgarrps@gmail.com>
2023-04-10 16:28:48 -0400

    video improt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
-
+import video from '../assets/public.mp4'
 export default function Home() {
   return (
 
     <div className='flex items-center justify-center bg-black h-screen'>
-      <video className='absolute w-full h-full object-cover opacity-50' src='public.mp4' muted loop autoPlay />
+      <video className='absolute w-full h-full object-cover opacity-50' src={video} muted loop autoPlay />
       <Link className='text-white w-[150px] text-center font-bold rounded-md border-2 hover:ease-in-out duration-300 hover:shadow-green-500/50 hover:border-emerald-400/100 hover:border-2 hover:shadow-lg' href='/wallet'>Let's get started</Link>
     </div>
   )
