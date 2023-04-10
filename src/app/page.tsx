10cce9d28e7e9e6c8de75ecebefe9714f049d39a
Edgar Santos <edgarrps@gmail.com>
2023-04-10 16:23:53 -0400

    video path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
-import wallet from './wallet/page'
 
 export default function Home() {
   return (
+
     <div className='flex items-center justify-center bg-black h-screen'>
+      <video className='absolute w-full h-full object-cover opacity-50' src='public.mp4' muted loop autoPlay />
       <Link className='text-white w-[150px] text-center font-bold rounded-md border-2 hover:ease-in-out duration-300 hover:shadow-green-500/50 hover:border-emerald-400/100 hover:border-2 hover:shadow-lg' href='/wallet'>Let's get started</Link>
     </div>
   )
