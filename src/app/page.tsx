f9858b8fc4846143faaf6fac28905d3f501945c4
edgarrps <edgarrps@gmail.com>
2023-04-11 04:50:52 -0400

    intro page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
 
-    <div className='flex items-center justify-center bg-black h-screen'>
-      <source src={'../assets/public.mp4'} type="video/mp4" className='absolute w-full h-full object-cover opacity-50'  />
-      <Link className='text-white w-[150px] text-center font-bold rounded-md border-2 hover:ease-in-out duration-300 hover:shadow-green-500/50 hover:border-emerald-400/100 hover:border-2 hover:shadow-lg' href='/wallet'>Let's get started</Link>
+    <div className='flex flex-col items-center justify-center bg-black h-screen space-y-10'>
+      <h1 className='text-9xl text-gray-700 font-black text-center'><span className='bg-gradient-to-r from-emerald-500 to-purple-500 text-transparent bg-clip-text'>Ethereum Wallet: personal, pratice and private</span></h1>
+      <div><Link className='text-white font-bold rounded-md border-2 mx-auto w-[150px] hover:ease-in-out duration-300 hover:shadow-green-500/50 hover:border-emerald-400/100 hover:border-2 hover:shadow-lg' href='/wallet'>Let's get started</Link></div>
     </div>
   )
 }
