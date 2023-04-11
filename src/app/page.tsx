792879d8815aab4c2de7b523b276105a8829fba4
edgarrps <edgarrps@gmail.com>
2023-04-11 06:31:46 -0400

    style initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
 'use client'
 import Link from 'next/link'
 import { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { pageStyle, linkStyle, spanText, text } from './styles'
+
 export default function Home() {
-
-  const [visible, setVisible] = useState(false)
-  const render = () => { return visible && TextEth() }
-
-  setTimeout(() => {
-    setVisible(true)
-  }, 50)
+  
+  const [visibleText, setVisibleText] = useState(false)
+  const [visibleButton, setVisibleButton] = useState(false)
+  const renderText = () => { return visibleText && TextEth() }
+  setTimeout(() => { setVisibleText(true) }, 50)
+  const renderButton = () => { return visibleButton && <p className={twMerge(linkStyle, 'fadein')}><Link href='/wallet'>Let's get started</Link></p> }
+  setTimeout(() => { setVisibleButton(true) }, 1000)
 
 
   return (
-
-    <div className='flex flex-col items-center justify-center bg-black h-screen space-y-10'>
-      <div>{render()}</div>
-      <div><Link className='text-white font-bold rounded-md border-2 mx-auto w-[150px] hover:ease-in-out duration-300 hover:shadow-green-500/50 hover:border-emerald-400/100 hover:border-2 hover:shadow-lg' href='/wallet'>Let's get started</Link></div>
+    <div className={pageStyle}>
+      <p>{renderText()}</p>
+      {renderButton()}
     </div>
   )
 }
 
-export const TextEth = () => {
-  return <h1 className='text-9xl text-gray-700 font-black text-center'><span className='bg-gradient-to-r from-emerald-500 to-purple-500 text-transparent bg-clip-text'>Ethereum Wallet: personal, pratice and private</span></h1>
-
-}
+export const TextEth = () => {return <h1 className={text}><span className={twMerge(spanText, 'fadein')}>Ethereum Wallet: personal, pratice and private</span></h1>}
